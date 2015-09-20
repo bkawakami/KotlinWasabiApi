1e6acc7e64dff1d15aca29f31f15f216d53e8757
Bruno Kawakami <bru.kawakami@gmail.com>
2015-09-20 20:33:58 -0300

    Add JSON return :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,27 @@
+import org.avaje.agentloader.AgentLoader
 import org.wasabi.app.AppServer
 
 /**
  * Created by bruno on 9/19/15.
  */
 public object Main {
-    @JvmStatic fun main(args: Array<String>){
-        var server = AppServer()
+    @JvmStatic fun main(args: Array<String>) {
+
+        val server = AppServer()
+
+        server.configuration
 
         server.get("/", { response.send("Hello World on Wasabi!") })
 
+        server.get("/hello", {
+            var x = hashMapOf(Pair("hello", "world"), Pair("on", "wasabi"))
+            response.send(x, "application/json")
+        })
+
+        server.get("/get/:id", { response.send(request.routeParams["id"].toString()) })
+
         server.start()
     }
+
+
 }
